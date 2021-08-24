88a404a5a6632d1e5d0c148e09fc376d88b00b28
Joshua Xing <joshua.xing@hotmail.com>
2021-08-24 21:35:18 +1000

    add data model for card

---
+++ src/main/java/org/example/model/Card.java
@@ -0,0 +1,57 @@
+package org.example.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Joshua Xing
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Card implements Comparable<Card> {
+    private static final Pattern VALID_CARD_PATTERN = Pattern.compile("^([1-9TJKQA])([DHSC])$");
+
+    private final int value;
+    private final char suit;
+
+    @Override
+    public int compareTo(Card o) {
+        return this.value - o.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value) + suit;
+    }
+
+    public static Card from(String cardStr) {
+        Matcher matcher = VALID_CARD_PATTERN.matcher(cardStr);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid card: " + cardStr);
+        }
+        String valueStr = matcher.group(1);
+        String suitStr = matcher.group(2);
+        return new Card(valueStringToValue(valueStr), suitStr.charAt(0));
+    }
+
+    private static int valueStringToValue(String valueStr) {
+        switch (valueStr) {
+            case "T":
+                return 10;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            case "A":
+                return 14;
+            default:
+                return Integer.parseInt(valueStr);
+        }
+    }
+}
---
+++ src/test/java/org/example/model/CardTest.java
@@ -0,0 +1,31 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Joshua Xing
+ */
+class CardTest {
+    @Test
+    void fromTest() {
+        assertEquals("4H", Card.from("4H").toString());
+        assertEquals("10D", Card.from("TD").toString());
+        assertEquals("11C", Card.from("JC").toString());
+        assertEquals("12S", Card.from("QS").toString());
+        assertEquals("13H", Card.from("KH").toString());
+        assertEquals("14C", Card.from("AC").toString());
+    }
+
+    @Test
+    void fromInvalidCardTest() {
+        assertThrows(IllegalArgumentException.class, () -> Card.from("0H"), "Invalid card: 0H");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("2M"), "Invalid card: 2M");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("0M"), "Invalid card: 0M");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("11D"), "Invalid card: 11D");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("aD"), "Invalid card: aD");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("9c"), "Invalid card: 9c");
+        assertThrows(IllegalArgumentException.class, () -> Card.from("A"), "Invalid card: A");
+    }
+}
